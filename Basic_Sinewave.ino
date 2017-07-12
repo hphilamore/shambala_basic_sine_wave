005c6820dc851a0e9df84b48d96118f9d75f9519
hphilamore <hemma@Hemmas-MacBook-Pro.local>
2017-07-13 00:21:55 +0900

    cap sense changes frequency of wave

--- Basic_Sinewave.ino
+++ Basic_Sinewave.ino
@@ -17,9 +17,13 @@
 #include <Oscil.h> // oscillator template
 #include <tables/sin2048_int8.h> // sine table for oscillator
 #include <AutoMap.h>
+#include <RollingAverage.h>
+#include <CapacitiveSensor.h>
 
 // use: Oscil <table_size, update_rate> oscilName (wavetable), look in .h file of table #included above
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin(SIN2048_DATA);
+
+CapacitiveSensor   cs_3_4 = CapacitiveSensor(3,4); 
 
 // use #define for CONTROL_RATE, not a constant
 #define CONTROL_RATE 64 // powers of 2 please
@@ -29,10 +33,17 @@ const int KNOB_PIN = 0;
 const int MIN_IN = 0;
 const int MAX_IN = 1023;
 
+const int MIN_C = 600;
+const int MAX_C = 1200;
+
 const int MIN_F = 400;
 const int MAX_F = 1200;
 
 AutoMap kMapF(MIN_IN,MAX_IN,MIN_F,MAX_F);
+AutoMap kMapC(MIN_C,MAX_C,MIN_F,MAX_F);
+
+RollingAverage <int, 16> FAverage; 
+RollingAverage <int, 16> CapAverage; 
 
 void setup(){
   startMozzi(CONTROL_RATE); // set a control rate of 64 (powers of 2 please)
@@ -42,19 +53,27 @@ void setup(){
 
 
 void updateControl(){
-  // put changing controls in here
-  int fundamental = mozziAnalogRead(KNOB_PIN)+1;
-  //fundamental = FAverage.next(fundamental);
-//  Serial.print(fundamental);
-//    Serial.print("  ");
-  fundamental = kMapF(fundamental);
-//  Serial.print(fundamental);
-//    Serial.print("  ");
+  
+int fundamental = mozziAnalogRead(KNOB_PIN)+1;
+//fundamental = FAverage.next(fundamental);
+fundamental = kMapF(fundamental);
+Serial.print(fundamental);
+Serial.print("  ");
+
+long capsense1_ =  cs_3_4.capacitiveSensor(1);
+int capsense1 = (int) capsense1_;
+capsense1 = CapAverage.next(capsense1);
+capsense1 = kMapC(capsense1);
+//Serial.print(capsense1_);
+//Serial.print("  ");
+Serial.print(capsense1);
+Serial.println("  ");
+  
   aSin.setFreq(fundamental); // set the frequency
-  //aSin.setFreq(440); // set the frequency
+  aSin.setFreq(capsense1); // set the frequency
     float R = aSin.next();
-    Serial.print(R);
-    Serial.println("  ");
+//    Serial.print(R);
+//    Serial.println("  ");
 }
 
 
